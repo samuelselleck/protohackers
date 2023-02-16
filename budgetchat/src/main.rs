0791dd3a29300e1c11a4c6837042a16f0af75529
Samuel Selleck <sellecksamuel@gmail.com>
2023-02-17 00:54:45 +0100

    chat final?

--- budgetchat/src/main.rs
+++ budgetchat/src/main.rs
@@ -1,54 +1,78 @@
+use std::collections::HashSet;
 use std::error::Error;
 use std::io;
+use std::sync::Arc;
 use tokio::io::AsyncBufReadExt;
-use tokio::io::AsyncReadExt;
 use tokio::io::AsyncWriteExt;
 use tokio::io::BufReader;
-use tokio::net::tcp::OwnedWriteHalf;
 use tokio::net::TcpListener;
 use tokio::net::TcpStream;
 use tokio::select;
 use tokio::sync::broadcast;
-use tokio::sync::broadcast::Receiver;
 use tokio::sync::broadcast::Sender;
+use tokio::sync::Mutex;
 
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
 struct ChatRoom {
     channel: Sender<String>,
+    users: Arc<Mutex<HashSet<String>>>,
 }
 
 impl ChatRoom {
     fn new() -> Self {
         Self {
             channel: broadcast::channel(16).0,
+            users: Default::default(),
         }
     }
 
     fn new_user(&mut self) -> ChatRoomConnection {
         let sender = self.channel.clone();
-        ChatRoomConnection { sender }
+        let users = self.users.clone();
+        ChatRoomConnection { sender, users }
     }
 }
 
 struct ChatRoomConnection {
     sender: Sender<String>,
+    users: Arc<Mutex<HashSet<String>>>,
 }
 
 impl ChatRoomConnection {
     async fn handle_events(self, stream: TcpStream) -> Result<()> {
         let (reader, mut writer) = stream.into_split();
         let mut lines = BufReader::new(reader).lines();
+        writer
+            .write_all("Welcome to budgetchat! What shall I call you?".as_bytes())
+            .await?;
         let name = lines
             .next_line()
             .await?
             .ok_or("connection closed before name specified")?;
-        //TODO verify name has valid symbols (small/large letters and numbers) and length >= 1
+        let correct = name.len() >= 1 && name.chars().all(|c| c.is_alphanumeric());
+        if !correct {
+            writer
+                .write_all("please provide a valid name (at least 1 characater long, alpha-numeric characters.)".as_bytes())
+                .await?;
+            return Err("not valid name".into());
+        }
         self.sender
             .send(format!("* {} joined the chat\n", &name))
             .unwrap_or_default();
         println!("{} joined the chat", &name);
         let mut reciever = self.sender.subscribe();
+        let active_users = async {
+            let mut users = self.users.lock().await;
+            let active_users: Vec<_> = users.iter().cloned().collect();
+            users.insert(name.clone());
+            active_users
+        }
+        .await;
+        writer
+            .write_all(format!("* current users: {}", active_users.join(", ")).as_bytes())
+            .await?;
+
         loop {
             select! {
                 Ok(l) = lines.next_line() => {
@@ -69,6 +93,10 @@ impl ChatRoomConnection {
         }
         println!("{} left the chat", &name);
         self.sender.send(format!("* {} left the chat\n", &name))?;
+        {
+            let mut users = self.users.lock().await;
+            users.remove(&name);
+        }
         Ok(())
     }
 }
