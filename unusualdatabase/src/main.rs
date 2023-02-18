4a47b20470eaa42e633447b1c86eb9d78aabb5ea
Samuel Selleck <sellecksamuel@gmail.com>
2023-02-19 00:12:30 +0100

    version num

--- unusualdatabase/src/main.rs
+++ unusualdatabase/src/main.rs
@@ -7,28 +7,32 @@ type Result<T> = std::result::Result<T, Box<dyn Error>>;
 async fn main() -> Result<()> {
     let socket = UdpSocket::bind("0.0.0.0:8080").await?;
     let mut storage = HashMap::<String, String>::new();
+    storage.insert("version".into(), "samuels KV store v 1.0".into());
 
     loop {
         let mut buf = [0; 1024];
-        println!("waiting for recieve...");
+        // println!("waiting for recieve...");
         let (n, sender) = socket.recv_from(&mut buf).await?;
         let msg = String::from_utf8(buf[..n].to_vec())?;
-        println!("recieved: {:?}", &msg);
+        // println!("recieved: {:?}", &msg);
         let ind = msg.find('=');
         match ind {
             Some(i) => {
                 let (key, value) = msg.split_at(i);
-                println!("inserting {:?} = {:?}", key, &value[1..]);
+                // println!("inserting {:?} = {:?}", key, &value[1..]);
+                if key == "version" {
+                    continue;
+                }
                 storage.insert(key.into(), value[1..].into());
             }
             None => {
-                println!("message: {:?}", msg);
+                // println!("message: {:?}", msg);
                 if let Some(v) = storage.get(msg.as_str()) {
                     let response = format!("{}={}", msg, v);
-                    println!("sending response: {:?}", response);
+                    // println!("sending response: {:?}", response);
                     socket.send_to(response.as_bytes(), sender).await?;
                 } else {
-                    println!("no such key");
+                    // println!("no such key");
                 }
             }
         }
