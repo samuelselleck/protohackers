50519bb8b5bd8046db2b4f519f0a11552aae907d
Samuel Selleck <sellecksamuel@gmail.com>
2023-02-20 01:31:55 +0100

    verbose log

--- 5_mob_in_the_middle/src/main.rs
+++ 5_mob_in_the_middle/src/main.rs
@@ -3,6 +3,8 @@ use std::io;
 use tokio::io::AsyncBufReadExt;
 use tokio::io::AsyncWriteExt;
 use tokio::io::BufReader;
+use tokio::net::tcp::OwnedReadHalf;
+use tokio::net::tcp::OwnedWriteHalf;
 use tokio::net::TcpListener;
 use tokio::net::TcpStream;
 
@@ -11,17 +13,20 @@ type Result<T> = std::result::Result<T, Box<dyn Error>>;
 #[tokio::main]
 async fn main() -> io::Result<()> {
     let listener = TcpListener::bind("0.0.0.0:8080").await?;
+    let mut counter: i32 = 0;
     loop {
         let (socket, _) = listener.accept().await?;
-        tokio::spawn(async {
+        counter += 1;
+        let id = counter;
+        tokio::spawn(async move {
             println!("connection established");
-            replacing_proxy(socket).await.expect("closed unexpectedly");
+            let _ = replacing_proxy(socket, id).await;
             println!("connection closed");
         });
     }
 }
 
-async fn replacing_proxy(victim_connection: TcpStream) -> Result<()> {
+async fn replacing_proxy(victim_connection: TcpStream, id: i32) -> Result<()> {
     let addr = "chat.protohackers.com:16963";
     let chat_connection = TcpStream::connect(addr).await?;
     let (vic_r, mut vic_w) = victim_connection.into_split();
@@ -31,40 +36,34 @@ async fn replacing_proxy(victim_connection: TcpStream) -> Result<()> {
     println!("entered");
     loop {
         println!("mess");
-        let mut buf_vic = Vec::new();
-        let mut buf_chat = Vec::new();
         tokio::select! {
-            res  = vic_r.read_until(b'\n', &mut buf_vic) => {
-                println!("recv vic {:?}", buf_vic);
-                let n = res?;
-                if n == 0 || buf_vic.last().unwrap() != &b'\n' {
-                    break
-                }
-                let vic_message = String::from_utf8(buf_vic)?;
-                println!("message from vic: {:?}", vic_message);
-                let mut ret = replace_bogus(vic_message);
-                ret.push('\n');
-                println!("send vic -> serv {:?}", ret.as_bytes());
-                chat_w.write_all(ret.as_bytes()).await?;
-            },
-            res = chat_r.read_until(b'\n', &mut buf_chat) => {
-                println!("recv serv {:?}", buf_chat);
-                let n = res?;
-                if n == 0 || buf_chat.last().unwrap() != &b'\n' {
-                    break
-                }
-                let chat_message = String::from_utf8(buf_chat)?;
-                println!("message from server: {:?}", chat_message);
-                let mut ret = replace_bogus(chat_message);
-                ret.push('\n');
-                println!("send serv -> vic {:?}", ret.as_bytes());
-                vic_w.write_all(ret.as_bytes()).await?;
-            },
-            else => {
-                break
-            }
+            res = rewrite_line(id, "victim", &mut vic_r, "chat server", &mut chat_w) => { res? },
+            res = rewrite_line(id, "chat server", &mut chat_r, "victim", &mut vic_w) => { res? }
         }
     }
+}
+
+async fn rewrite_line(
+    connection_id: i32,
+    from_ident: &str,
+    from: &mut BufReader<OwnedReadHalf>,
+    to_ident: &str,
+    to: &mut OwnedWriteHalf,
+) -> Result<()> {
+    let mut buf = Vec::new();
+    let n = from.read_until(b'\n', &mut buf).await?;
+    if n == 0 || buf.last().unwrap() != &b'\n' {
+        return Err("end".into());
+    }
+    let message = String::from_utf8(buf)?;
+    println!(
+        "{}: recv ({: <12}): {:?}",
+        connection_id, from_ident, message
+    );
+    let mut ret = replace_bogus(message);
+    ret.push('\n');
+    println!("{}: sent ({: <12}): {:?}", connection_id, to_ident, ret);
+    to.write_all(ret.as_bytes()).await?;
     Ok(())
 }
 
