020f1550f927d9391e16bebbc2021718b20a9a1b
Samuel Selleck <sellecksamuel@gmail.com>
2023-02-25 01:46:55 +0100

    working?

--- 6_speed_daemon/src/main.rs
+++ 6_speed_daemon/src/main.rs
@@ -1,22 +1,23 @@
 mod messages;
 
+use async_channel;
 use dashmap::DashMap;
 use futures::sink::SinkExt;
 use futures::stream::select_all;
+use futures::stream::FuturesUnordered;
 use futures::StreamExt;
 use messages::*;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::io;
+use std::pin::Pin;
 use std::sync::Arc;
 use std::time::Duration;
 use tokio::net::TcpListener;
 use tokio::sync::mpsc;
 use tokio::sync::mpsc::Receiver;
 use tokio::sync::mpsc::Sender;
-use tokio_stream::wrappers::ReceiverStream;
 use tokio_util::codec::Framed;
-
 const CHANNEL_SIZE: usize = 10000;
 
 #[derive(Debug)]
@@ -35,18 +36,60 @@ struct RoadNetwork {
 
 struct Road {
     speed_limit: Option<Speed>,
-    rx_road: Option<Receiver<Sighting>>,
+    rx_road: async_channel::Receiver<ServerMessage>,
     tx_road: Sender<Sighting>,
 }
 
 impl Default for Road {
     fn default() -> Self {
         let (tx_road, rx_road) = mpsc::channel(CHANNEL_SIZE);
+        let (tx_road_ticket, rx_road_ticket) = async_channel::unbounded();
+        tokio::spawn(async move { Self::road_ticket_handler(rx_road, tx_road_ticket).await });
         Road {
             speed_limit: None,
-            rx_road: Some(rx_road),
+            rx_road: rx_road_ticket,
             tx_road,
         }
+    }
+}
+
+impl Road {
+    async fn road_ticket_handler(
+        mut rx_road: Receiver<Sighting>,
+        tx_road_ticket: async_channel::Sender<ServerMessage>,
+    ) -> Option<()> {
+        let mut log = HashMap::<RoadID, HashMap<Plate, Vec<(Mile, TimeStamp)>>>::new();
+        let mut ticket_days = HashMap::<Plate, HashSet<u32>>::new();
+        while let Some(Sighting {
+            road: road_id,
+            speed_limit: road_speed_limit,
+            plate,
+            position: mile,
+            time,
+        }) = rx_road.recv().await
+        {
+            let road_sightings = log
+                .entry(road_id)
+                .or_default()
+                .entry(plate.clone())
+                .or_default();
+
+            road_sightings.push((mile, time));
+            let car_ticket_days = ticket_days.entry(plate.clone()).or_default();
+            if let Some((p1, p2, speed)) =
+                should_be_ticketed(road_sightings, road_speed_limit, car_ticket_days)
+            {
+                let ticket = ServerMessage::Ticket {
+                    plate,
+                    road: road_id,
+                    p1,
+                    p2,
+                    speed,
+                };
+                tx_road_ticket.send(ticket).await.ok()?;
+            }
+        }
+        Some(())
     }
 }
 
@@ -57,16 +100,17 @@ impl RoadNetwork {
         road.tx_road.clone()
     }
 
-    fn register_dispatcher(&self, roads: Vec<RoadID>) -> impl StreamExt<Item = Sighting> + Unpin {
-        let mut streams = Vec::new();
+    fn register_dispatcher(
+        &self,
+        roads: Vec<RoadID>,
+    ) -> impl StreamExt<Item = ServerMessage> + Unpin {
+        let mut all = Vec::new();
         for road in roads {
-            let mut road = self.roads.entry(road).or_default();
-            if let Some(rx_road) = road.rx_road.take() {
-                let stream = ReceiverStream::new(rx_road);
-                streams.push(stream);
-            }
+            let road = self.roads.entry(road).or_default();
+            let rx_road = road.rx_road.clone();
+            all.push(rx_road);
         }
-        select_all(streams)
+        select_all(all)
     }
 }
 
@@ -210,39 +254,11 @@ async fn handle_main(
 
 async fn handle_dispatcher(
     id: i32,
-    mut stream: impl StreamExt<Item = Sighting> + Unpin,
+    mut stream: impl StreamExt<Item = ServerMessage> + Unpin,
     tx_out: Sender<ServerMessage>,
 ) -> Option<()> {
-    let mut log = HashMap::<RoadID, HashMap<Plate, Vec<(Mile, TimeStamp)>>>::new();
-    let mut ticket_days = HashMap::<Plate, HashSet<u32>>::new();
-    while let Some(Sighting {
-        road: road_id,
-        speed_limit: road_speed_limit,
-        plate,
-        position: mile,
-        time,
-    }) = stream.next().await
-    {
-        let road_sightings = log
-            .entry(road_id)
-            .or_default()
-            .entry(plate.clone())
-            .or_default();
-
-        road_sightings.push((mile, time));
-        let car_ticket_days = ticket_days.entry(plate.clone()).or_default();
-        if let Some((p1, p2, speed)) =
-            should_be_ticketed(road_sightings, road_speed_limit, car_ticket_days)
-        {
-            let ticket = ServerMessage::Ticket {
-                plate,
-                road: road_id,
-                p1,
-                p2,
-                speed,
-            };
-            tx_out.send(ticket).await.ok()?;
-        }
+    while let Some(msg) = stream.next().await {
+        tx_out.send(msg).await.ok()?
     }
     Some(())
 }
